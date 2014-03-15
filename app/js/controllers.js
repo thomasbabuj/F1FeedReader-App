e871ebc04bfbcb0db5a66783e37ce48fd56d6083
Thomas Babu <tommy.developer@gmail.com>
2014-03-15 13:21:40 +0800

    Added a service which fetch data from ergastAPIservice and added filters

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,22 @@
 'use strict';
 
-/* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+angular.module('F1FeederApp.controllers', []).
+	
+	  controller('driversController', function($scope, ergastAPIservice) {		
+		$scope.nameFilter = null;
+		$scope.driversList = [];
 
-  }])
-  .controller('MyCtrl2', [function() {
+		$scope.searchFilter = function(driver) {
+			
+			var keyword = new RegExp( $scope.nameFilter, 'i');
 
-  }]);
+			return !$scope.nameFilter || keyword.test(driver.Driver.givenName) || keyword.test (driver.Driver.familyName);
+		}
+
+		ergastAPIservice.getDrivers().success(function (response){			
+			$scope.driversList = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+		});
+	});
+
+	  
